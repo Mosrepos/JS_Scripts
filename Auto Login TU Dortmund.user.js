a21dccc0fd580e298a68a835b966aeb0288da26b
Mo Abdo <mo.abdo_social@icloud.com>
2024-05-29 20:14:18 +0200

    url

--- Auto Login TU Dortmund.user.js
+++ Auto Login TU Dortmund.user.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  Script to auto login into moodle of the TU Dortmund
 // @author       Mohamad Abdo
-// @match        https://*.tu-dortmund.de/*
+// @match        https://moodle.tu-dortmund.de/*
+// @match        https://sso.itmc.tu-dortmund.de/*
 // @grant        none
 // ==/UserScript==
 (function() {
